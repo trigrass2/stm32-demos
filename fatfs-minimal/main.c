4fb440289b25ed6a23638de0da6a764882ccc2b0
drmetal <spaceorbot@gmail.com>
2014-12-01 21:30:04 +1300

    minimal fatfs project now works

--- fatfs-minimal/main.c
+++ fatfs-minimal/main.c
@@ -1,55 +1,72 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "leds.h"
-#include "ff.h"
+#include "sdfs.h"
 
 #define TEST_FILENAME   "testfile"
-FATFS fs;
-char buffer[16];
+#define TEST_FLASHES   "10"
+
+
+typedef struct {
+    FATFS fs;
+    char buffer[16];
+    FIL file;
+    int flash;
+    int count;
+}appdata_t;
+
+/**
+ * put the struct in bss - in my setup the stack is located in CCRAM on stm32f4 devices.
+ * the filesystem uses DMA, which cant be used with CCRAM.
+ */
+appdata_t appdata = {
+    .flash = 1,             // default to flash on once
+};
 
 int main(void)
 {
-    // default to flash on once
-    int flash = 2;
     int i;
-    FIL f;
 
-    f_mount(0, &fs);
-
-    // open a new file for writing
-    if(f_open(&f, TEST_FILENAME, FA_WRITE|FA_OPEN_ALWAYS) == FR_OK)
+    if(sdfs_init())
     {
-        // flash twice
-        flash = 4;
-        // write a number to the file (no newline needed in this case)
-        f_puts((const TCHAR *)"12", &f);
-        f_close(&f);
-    }
-
-    // open the new file for reading
-    if(f_open(&f, TEST_FILENAME, FA_READ) == FR_OK)
-    {
-        // flash three times
-        flash = 6;
-
-        if(f_gets((TCHAR *)buffer, sizeof(buffer)-1, &f))
+        // open a new file for writing
+        if(f_open(&appdata.file, TEST_FILENAME, FA_WRITE|FA_OPEN_ALWAYS) == FR_OK)
         {
-            // flash the number of times set in the file
-            // decode the number in the file
-            flash = atoi(buffer);
+
+            // flash twice
+            appdata.flash = 2;
+            // write a number to the file (no newline needed in this case)
+            f_puts((const TCHAR *)TEST_FLASHES, &appdata.file);
+            f_close(&appdata.file);
         }
-        f_close(&f);
+
+        // open the new file for reading
+        if(f_open(&appdata.file, TEST_FILENAME, FA_READ) == FR_OK)
+        {
+            // flash three times
+            appdata.flash = 3;
+
+            if(f_gets((TCHAR *)appdata.buffer, sizeof(appdata.buffer)-1, &appdata.file))
+            {
+                // flash the number of times set in the file
+                // decode the number in the file
+                appdata.flash = atoi(appdata.buffer);
+            }
+            f_close(&appdata.file);
+        }
     }
 
     while(1)
     {
-        sleep(3);
-
-        for(i = 0; i < flash; i++)
+        for(i = 0; i < appdata.flash; i++)
         {
-            usleep(250000);
-            toggle_led(LED1);
+            set_led(LED1);
+            usleep(100000);
+            clear_led(LED1);
+            usleep(200000);
         }
+
+        sleep(1);
     }
 
     return 0;

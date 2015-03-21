b9cd7c1cab6a145af8890bbc370567b3ed99065a
mike <spaceorbot@gmail.com>
2015-03-21 22:37:43 +1300

    tweaked http server demo lwipopts, works a little better now

--- http-server/lwipopts.h
+++ http-server/lwipopts.h
@@ -66,10 +66,10 @@ a lot of data that needs to be copied, this should be set high. */
 #define MEMP_NUM_UDP_PCB        6
 /* MEMP_NUM_TCP_PCB: the number of simulatenously active TCP
    connections. */
-#define MEMP_NUM_TCP_PCB        10
+#define MEMP_NUM_TCP_PCB        5
 /* MEMP_NUM_TCP_PCB_LISTEN: the number of listening TCP
    connections. */
-#define MEMP_NUM_TCP_PCB_LISTEN 6
+#define MEMP_NUM_TCP_PCB_LISTEN 5
 /* MEMP_NUM_TCP_SEG: the number of simultaneously queued TCP
    segments. */
 #define MEMP_NUM_TCP_SEG        12
@@ -77,6 +77,9 @@ a lot of data that needs to be copied, this should be set high. */
    timeouts. */
 #define MEMP_NUM_SYS_TIMEOUT    10
 
+/* MEMP_NUM_NETCONN: the number of simulateously active
+   netconns. */
+#define MEMP_NUM_NETCONN        8
 
 /* ---------- Pbuf options ---------- */
 /* PBUF_POOL_SIZE: the number of buffers in the pbuf pool. */

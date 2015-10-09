dfc36a81fe43ba3c803f83d4ff1bbe827db20ca8
mike <spaceorbot@gmail.com>
2015-10-10 12:39:46 +1300

    refactored to use new appleseed minlibc structure

--- adc-stream/main.c
+++ adc-stream/main.c
@@ -6,9 +6,16 @@
 #include "leds.h"
 #include "adc_stream.h"
 
+/********************
+ *
+ * demo is broken :(
+ *
+ ********************/
+
 void adc_stream_callback(uint16_t* buffer, uint16_t length, uint8_t channels, stream_connection_t* conn)
 {
-    printf("%s: %x %d %d %d\n", conn->name, buffer, length, channels, conn->stream_channel);
+    const char* appdata = (const char*)conn->ctx;
+    printf("%s: %d %x %d %d %d (%s)\n", conn->name, *buffer, buffer, length, channels, conn->stream_channel, appdata);
 }
 
 stream_connection_t adc_stream_conn;
@@ -18,18 +25,19 @@ void test_task(void*p)
     (void)p;
 
     adc_stream_init();
-    adc_stream_set_samplerate(2000);
     adc_stream_start();
-    stream_connection_init(&adc_stream_conn, adc_stream_callback, "adc process", NULL);
+
+    adc_stream_set_samplerate(2000);
+
+    stream_connection_init(&adc_stream_conn, adc_stream_callback, "adc process", "some app context data");
     adc_stream_connect_service(&adc_stream_conn, 0);
-    stream_connection_enable(&adc_stream_conn, true);
 
     for(;;)
     {
         toggle_led(LED2);
-        adc_stream_start();
+        stream_connection_enable(&adc_stream_conn, true);
         sleep(1);
-        adc_stream_stop();
+        stream_connection_enable(&adc_stream_conn, false);
         sleep(1);
     }
 }
